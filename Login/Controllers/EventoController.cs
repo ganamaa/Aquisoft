32b80e9da6d01d078e5554412c0f7cd858c12ad4
ana garcia <ganamaa@hotmail.es>
2020-03-16 10:54:25 -0500

    Aquisoft

---
+++ Login/Controllers/EventoController.cs
@@ -0,0 +1,49 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class EventoController : Controller
+    {
+        // GET: Evento
+        public ActionResult Evento()
+        {
+            using (EventoContext db = new EventoContext())
+            {
+                return View(db.Lugar.ToList());
+            }
+       
+        }
+
+        
+        public ActionResult CrearEvento()
+        {
+                return View();
+        }
+        
+        [HttpPost]
+        public ActionResult CrearEvento(Lugar a)
+        {
+            try
+            {
+                using (var db = new EventoContext())
+                {
+                    db.Lugar.Add(a);
+                    db.SaveChanges();
+                    return RedirectToAction("Evento");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error al agregar el evento", ex);
+                return View();
+            }
+        }
+    }
+
+}
