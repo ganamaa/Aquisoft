01b818038894774b577793c38cd87a642dc0e824
ana garcia <ganamaa@hotmail.es>
2020-03-21 19:10:11 -0500

    se actualizo el modelo y crearEvento

---
+++ Login/Models/LugarCE.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Login.Datos
+{
+    public class LugarCE
+    {
+        [Required]
+        [Display]
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
+        public string Usuario { get; set; }
+        [Required]
+        [Display]
+        public string Categoria { get; set; }
+        [DataType(DataType.Date), Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> FechaInicio { get; set; }
+        [DataType(DataType.Date), Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> FechaFin { get; set; }
+        [Required]
+        [Display]
+        public Nullable<int> Capacidad { get; set; }
+        [Required]
+        [Display]
+        public Nullable<int> Asistentes { get; set; }
+    }
+    [MetadataType(typeof(LugarCE))]
+    public partial class Lugar
+    {   
+
+    }
+}
